066a31c5d71404369cc9169cf7b3bae428ce5f8b
jofri99 <joofriii@googlemail.com>
2019-11-02 19:18:14 +0100

    Add average vals

--- SAuND/Quelle.cpp
+++ SAuND/Quelle.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 using namespace cv;
 
+Mat LOWRES;
+
 VideoCapture getWebcam(int webcam) {
 	VideoCapture cap;
 	if (!cap.open(webcam)) {
@@ -14,12 +16,34 @@ VideoCapture getWebcam(int webcam) {
 	return cap;
 }
 
-void runVideo(float pixelScale,int step,String windowName,bool logConsole,bool showCam) {
+double getSquare(int x, int y, int rows, int cols) 
+{
+	int counter = 0;
+	double avg = 0;
+
+	for (int i =x; i < x + rows; i++) {
+		for (int j= y; j < y + cols; j++) {
+			avg += (int) LOWRES.at<uchar>(i, j);
+			counter++;
+		}
+		
+	}
+
+	avg = avg / counter;
+	return avg;
+}
+
+void runVideo(float pixelScale, String windowName, bool showCam) 
+{
 	VideoCapture cap = getWebcam(0);
 	int i = 0;
+	double * avgVals;
+	double rows = 3;
+	double cols = 4;
+	avgVals = (double*) malloc (rows * cols * sizeof(int));
 
 	for (;;) {
-		Mat frame, grey, lowRes;
+		Mat frame, grey;
 		cap >> frame;
 		if (frame.empty()) {
 			break;
@@ -29,35 +53,32 @@ void runVideo(float pixelScale,int step,String windowName,bool logConsole,bool s
 		cvtColor(frame, grey, COLOR_BGR2GRAY);
 		
 		//Resize image with given factor "pixelScale"
-		resize(grey, lowRes, Size(), pixelScale, pixelScale);
+		resize(grey, LOWRES, Size(), pixelScale, pixelScale);
 
 		if (showCam) {
 			namedWindow(windowName, 0);
 			resizeWindow(windowName, 640, 480);
-			imshow(windowName, lowRes);
+			imshow(windowName, LOWRES);
 		}
 
-		if(logConsole){
-			if (i > step) {
-				system("cls");
-				//output of greyscales
-				cout << lowRes << endl;
-				cout << lowRes.size << endl;
-				i = 0;
+		double avg = getSquare(0, 0, 8, 8);
+		system("cls");
+		for (int i = 0; i < 24; i += 8) {
+			cout << endl;
+			for (int j = 0; j < 32; j += 8) {
+				avg = getSquare(i, j, 8, 8);
+				cout << avg << "\t";
 			}
-			i++;
 		}
 
 		if (waitKey(10) == 27) { //Esc(27) stop capturing
 			break;
 		}
 	}
-
-
 }
 int main()
 {
-	runVideo(0.1,5,"Webcam",true,false);
+	runVideo(0.05,"Webcam",true);
 	return 0;
 }
 
